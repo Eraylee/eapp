3c2309dce895095d41c3009e308c91f0e36c95da
eraylee <eraylee@live.com>
2022-06-27 17:22:36 +0800

    feat: ✨ modifyVars

---
+++ apps/gallery-client/webpack.config.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge');
+
+module.exports = (config, context) => {
+  return merge(config, {
+    module: {
+      rules: [
+        {
+          test: /\.less$/i,
+          use: [
+            {
+              loader: 'less-loader',
+              options: {
+                lessOptions: {
+                  javascriptEnabled: true,
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+  });
+};
